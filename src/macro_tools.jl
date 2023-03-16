fe33831261eab8d1198515fafb728eeb63a0f73d
スノル <s@sunoru.com>
2023-03-16 23:45:18 +0000

    `@destruct`.

--- src/macro_tools.jl
+++ src/macro_tools.jl
@@ -1,71 +1,76 @@
-# https://github.com/thautwarm/MLStyle-Playground/blob/master/StaticCapturing.jl
-function get_bindings(pattern::Expr)
+function get_bindings(pattern::Expr, __module__::Module=Main)
     result = Set{Symbol}()
-    _analyze(pat, is_literal=true) = @match pat begin
-        ::QuoteNode => _analyze(pat.value, true)
-        if is_literal end && Expr(:$, args...) => foreach(x -> _analyze(x, false), args)
-        if is_literal end && Expr(_, args...) => foreach(_analyze, args)
-        if is_literal end && _ => nothing
-        ::Symbol => (push!(result, pat); nothing)
-        Expr(:quote, args...) => foreach(x -> _analyze(x, true), args)
-        :(Do($(args...))) => foreach(args) do arg
-            @match arg begin
-                Expr(:kw, key::Symbol, value) => push!(result, key)
-                _ => nothing
+    _analyze(pat, is_literal) = @switch pat begin
+        @case ::QuoteNode
+            _analyze(pat.value, true)
+        @case if is_literal end && Expr(:$, args...)
+            foreach(x -> _analyze(x, false), args)
+        @case if is_literal end && Expr(_, args...)
+            foreach(x -> _analyze(x, true), args)
+        @case if is_literal end && _
+        @case :_
+        @case ::Symbol && if isdefined(__module__, pat) && MLStyle.is_enum(getfield(__module__, pat)) end
+            # nothing
+        @case ::Symbol
+            push!(result, pat)
+        @case Expr(:quote, args...)
+            foreach(x -> _analyze(x, true), args)
+        @case :(Do($(args...)))
+            foreach(args) do arg
+                @when let Expr(:kw, key::Symbol, _) = arg
+                    push!(result, key)
+                end
             end
-        end
-        :($a || $b) => begin
+        @case :($a || $b)
             _analyze(a, false)
             _analyze(b, false)
-            nothing
-        end
+        # extract fields
+        @case Expr(:kw, _, v)
+            _analyze(v, false)
         # type pattern
-        Expr(:(::), v, _...) => _analyze(v, false)
+        @case Expr(:(::), v, _...)
+            _analyze(v, false)
         # dict pattern
-        :(Dict($(args...))) => foreach(args) do arg
-            @match arg begin
-                :($_ => $v) => _analyze(v, false)
-                _ => nothing
+        @case :(Dict($(args...)))
+            foreach(args) do arg
+                @when let :($_ => $v) = arg
+                    _analyze(v, false)
+                end
             end
-        end
         # app pattern
-        :($_($(args...))) => foreach(x -> _analyze(x, false), args)
+        @case :($_($(args...)))
+            foreach(x -> _analyze(x, false), args)
         # other expr
-        Expr(_, args...) => foreach(x -> _analyze(x, false), args)
-        _ => nothing
+        @case Expr(_, args...)
+            foreach(x -> _analyze(x, false), args)
+        @case _
     end
-    _analyze(pattern)
+    _analyze(pattern, false)
     result
 end
 
-const rmlines = @λ begin
-    ::LineNumberNode -> nothing
-    Expr(head, args...) -> Expr(head, filter(!isnothing, map(rmlines, args))...)
-    s -> s
-end
-
 """
-    @capture(expr, pattern)
+    @destruct lhs = rhs
 
-Capture and bind variables in `pattern` to `expr`.
-Similar to `MacroTools.@capture`, but using static analysis.
-
-It supports the syntax of `@match` and `@λ` in `MLStyle.jl`.
+Destruct `rhs` into `lhs`. Similar to `@when let lhs = rhs`, but the destructed
+values are available in the current scope, and it throws an error if the pattern
+does not match.
 """
-@public macro capture(expr, pattern)
-    bindings = get_bindings(pattern)
-    t = gensym()
+@public macro destruct(expr)
+    lhs, rhs = @match expr begin
+        Expr(:(=), lhs, rhs) => (lhs, rhs)
+        _ => error("Usage: @destruct lhs = rhs")
+    end
+    bindings = get_bindings(lhs, __module__)
+    binding_tuple = Expr(:tuple, bindings...)
     quote
-        $(esc(t)) = @match rmlines($(esc(expr))) begin
-            $(Expr(:quote, pattern)) => $(Expr(:tuple, bindings...))
-            _ => nothing
+        result = $(esc(rhs))
+        $(esc(binding_tuple)) = @when let $lhs = result
+            $(binding_tuple)
+        @otherwise
+            error("Failed to destruct: $(result)")
         end
-        if isnothing($(esc(t)))
-            false
-        else
-            $(esc(:($(Expr(:tuple, bindings...)) = $t)))
-            true
-        end
+        result
     end
 end
 
@@ -84,7 +89,7 @@ h(x::Foo, args...; kwargs...) = h(x.bar, args...; kwargs...)
 It is similar to `MacroTools.@forward`.
 """
 @public macro forward(member, methods)
-    @capture member $T.$field
+    @destruct :($T.$field) = member
     methods = @match methods begin
         Expr(:tuple, args...) => esc.(args)
         _ => [esc(methods)]
--- test/macro_tools.jl
+++ test/macro_tools.jl
@@ -1,37 +1,49 @@
 using Test
-using Kirei
+using Kirei.Common
+
+@as_record struct Foo1
+    x::Union{Foo1,String}
+end
+struct Foo2
+    s::String
+end
 
 @testset "Macro Tools" begin
-    @testset "@capture" begin
-        expr = quote
-            ff(1, "2")
-        end
-        let m = @capture expr begin
-                $(:($f = $args) || :($f($(args...))))
-            end
-            @test m
-            @test f ≡ :ff
-            @test args == [1, "2"]
-        end
+
+    @testset "@destruct" begin
+
+        @destruct (a, b, (c, d, (5,))) = (1, 2, (3, 4, (5,)))
+        @test (a, b, c, d) == (1, 2, 3, 4)
+
+        @destruct [1, pack..., t] = [1, 2, 3, 4]
+        @test pack == [2, 3]
+        @test t == 4
+
+        foo = Foo1(Foo1("abc"))
+        result = @destruct Foo1(x=Foo1(x=s)) = foo
+        @test result ≡ foo
+        @test s == "abc"
+        @test !isdefined(Main, :x)
 
         function f(expr)
-            m = @capture expr $(a::Symbol) + $(b::Symbol)
-            m, a, b
+            @destruct :($(sa::Symbol) + $(sb::Symbol)) = expr
+            sa, sb
         end
-        @inferred f(:(x + y))
-        @test !(@capture expr -$a)
+        expr = :(x + y)
+        @test f(expr) ≡ (:x, :y)
+        @inferred f(expr)
+
+        @test_throws ErrorException @destruct [x, y] = [1, 2, 3]
     end
 
     @testset "@forward" begin
-        struct Foo
-            s::String
-        end
-        @forward Foo.s Base.length, Base.getindex
-        foo = Foo("abc")
+        @forward Foo2.s Base.length, Base.getindex
+
+        foo = Foo2("abc")
         @test length(foo) ≡ 3
         @test foo[1] ≡ 'a'
         f(s::String) = s[2]
-        @forward Foo.s f
+        @forward Foo2.s f
         f(foo) ≡ 'b'
     end
 end
