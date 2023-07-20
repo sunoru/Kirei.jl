5119adb2fc312d29b296e4f6ad1c969f89bc2e99
スノル <s@sunoru.com>
2023-07-20 23:05:49 +0000

    `@variadic_ccall`.

--- src/tools/ffi.jl
+++ src/tools/ffi.jl
@@ -1,19 +1,26 @@
-function _kcall(lib, expr)
+function _parse_kcall(lib, expr; escape=true)
     f, args, T = @match expr begin
         Expr(:call, f, args...) => (f, args, :Cvoid)
-        Expr(:(::), Expr(:call, f, args...), T) => (f, args, esc(T))
+        Expr(:(::), Expr(:call, f, args...), T) => (f, args, escape ? esc(T) : T)
         _ => error("Invalid expression for @kcall: $expr")
     end
     if !isnothing(lib) && f isa Symbol
         f = :($lib.$f)
     end
-    for i in eachindex(args)
-        arg = args[i]
-        args[i] = @match arg begin
-            Expr(:(::), a, t) => :($(esc(a))::$(esc(t)))
-            ::Symbol => :($(esc(arg))::Ptr{Cvoid})
-        end
+    map_arg(arg) = @match arg begin
+        Expr(:parameters, varargs...) => Expr(:parameters, map(map_arg, varargs)...)
+        Expr(:(::), a, t) => escape ? :($(esc(a))::$(esc(t))) : :($a::$t)
+        ::Symbol => escape ? :($(esc(arg))::Ptr{Cvoid}) : :($arg::Ptr{Cvoid})
+        _ => arg
     end
+    for i in eachindex(args)
+        args[i] = map_arg(args[i])
+    end
+    (; f, args, T)
+end
+
+function _kcall(lib, expr)
+    f, args, T = _parse_kcall(lib, expr)
     :(@ccall $f($(args...))::$T)
 end
 
@@ -29,4 +36,61 @@ end
 
 macro kcall(expr)
     _kcall(nothing, expr)
+end
+
+function gen_variadic_ccall(lib, expr)
+    f, args, T = _parse_kcall(lib, expr, escape=false)
+    @destruct [Expr(:parameters, params...), _...] = args "Input is not a variadic call"
+    @destruct [_..., :($varargs_sym...)] = params "Input does not have varargs"
+
+    pos_args = args[2:end]
+    pos_params = params[1:end - 1]
+    body = QuoteNode(:(@ccall $f(
+        $(pos_args...);
+        $(pos_params...)
+    )::$T))
+
+    quote
+        varargs = $(esc(varargs_sym))
+        varargs = if varargs isa DataType && varargs <: Tuple
+            varargs.parameters
+        else
+            varargs
+        end
+        vararg_types = [
+            t <: AbstractString ? Cstring : t
+            for t in varargs
+        ]
+        len = length(varargs)
+        # Copy is needed here
+        body = copy($body)
+        params = body.args[3].args[1].args[2].args
+        for (i, type) in enumerate(vararg_types)
+            push!(params, $(Expr(
+                :quote,
+                :($varargs_sym[$(Expr(:$, :i))]::$(Expr(:$, :type)))
+            )))
+        end
+        body
+    end
+end
+
+"""
+    @variadic_ccall [lib] cfunc(arg0::T0, arg1::T1; varargs...)::TR
+
+Used in a `@generated` function to call a variadic C function.
+
+Only simple types are supported.
+
+Example:
+
+```julia
+@generated printf(fmt, varargs...) = @variadic_ccall printf(s::Cstring; varargs...)::Cint
+```
+"""
+@public macro variadic_ccall(lib, expr)
+    gen_variadic_ccall(lib, expr)
+end
+macro variadic_ccall(expr)
+    gen_variadic_ccall(nothing, expr)
 end
--- test/tools/ffi.jl
+++ test/tools/ffi.jl
@@ -1,6 +1,19 @@
 using Test
 using Kirei
 
+# `@generated` should be top-level due to world age issues
+@generated function sprintf(fmt, varargs...; buffer_size=30)
+    quote
+        buffer = zeros(UInt8, buffer_size)
+        len = $(@variadic_ccall sprintf(
+            buffer::Ptr{Cchar},
+            fmt::Cstring;
+            varargs...
+        )::Cint)
+        String(view(buffer, 1:len))
+    end
+end
+
 @testset "FFI tools" begin
 
     @testset "@kcall" begin
@@ -11,4 +24,9 @@ using Kirei
         @test 1 == @kcall :libjulia jl_ver_major()::Cint
     end
 
+    @testset "@variadic_ccall" begin
+        @test sprintf("Hello, %s!", "world") == "Hello, world!"
+        @test sprintf("%s, %d!", "Hello", 123) == "Hello, 123!"
+        @test sprintf("%.3f, %s, %d!", 0.1234, "Hello", 123) == "0.123, Hello, 123!"
+    end
 end
